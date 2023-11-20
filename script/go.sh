52e02ea8e6f4f1129f988bcb50b5569801f5c2cf
Reza Hidayat <m.rezahidayat.rh@gmail.com>
2023-11-20 10:31:25 +0700

    update script

--- script/go.sh
+++ script/go.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Langkah 1: Unduh file instalasi Golang
-wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
 
 # Langkah 2: Ekstrak file instalasi Golang
-sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
 
 # Langkah 3: Tambahkan variabel lingkungan Golang ke .bashrc
 echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
@@ -15,6 +15,20 @@ echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
 # Langkah 5: Hapus file instalasi yang sudah diunduh
-rm go1.21.0.linux-amd64.tar.gz
+rm go1.21.4.linux-amd64.tar.gz
+
+config_file=~/.config/fish/config.fish
+# Pastikan file konfigurasi fish ada
+if [ ! -f "$config_file" ]; then
+    echo "File konfigurasi $config_file tidak ditemukan. Buat file tersebut terlebih dahulu."
+    exit 1
+fi
+
+# Tambahkan konfigurasi ke file config.fish
+echo 'set -x GOROOT /usr/local/go' >> "$config_file"
+echo 'set -x GOPATH $HOME/go' >> "$config_file"
+echo 'set -x PATH $GOPATH/bin $GOROOT/bin $PATH' >> "$config_file"
+
+echo "Konfigurasi telah ditambahkan ke $config_file"
 
 echo "Instalasi Golang selesai."
