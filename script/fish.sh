ab794eae6c9db83d9816336089b2a74d65dbb086
Reza Hidayat <m.rezahidayat.rh@gmail.com>
2023-11-21 00:54:50 +0700

    final

--- script/fish.sh
+++ script/fish.sh
@@ -17,17 +17,17 @@ chmod 700 ~/.config/fish/config.fish
 chmod 700 ~/.config/fish/functions/fish_prompt.fish
 
 # Install Plugin dengan fish
-fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
-fish -c "fisher install jorgebucaran/nvm.fish"
-fish -c "fisher install jethrokuan/z"
-fish -c "fisher install takashabe/fish-peco"
-fish -c "fisher install edc/bass"
-fish -c "fisher install PatrickF1/fzf.fish"
-fish -c "fisher install simnalamburt/shellder"
-fish -c "sudo apt install exa"
-fish -c "sudo apt install peco"
-fish -c "sudo apt install fzf"
-fish -c "sudo apt install trash-cli"
+sudo fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+sudo fish -c "fisher install jorgebucaran/nvm.fish"
+sudo fish -c "fisher install jethrokuan/z"
+sudo fish -c "fisher install takashabe/fish-peco"
+sudo fish -c "fisher install edc/bass"
+sudo fish -c "fisher install PatrickF1/fzf.fish"
+sudo fish -c "fisher install simnalamburt/shellder"
+sudo fish -c "sudo apt install exa"
+sudo fish -c "sudo apt install peco"
+sudo fish -c "sudo apt install fzf"
+sudo fish -c "sudo apt install trash-cli"
 
 cp -i "$HOME/github/LINUX/fish/config.fish" "$HOME/.config/fish/"
 
@@ -37,11 +37,11 @@ echo "Script selesai dijalankan."
 # git clone https://github.com/oh-my-fish/oh-my-fish
 # cd oh-my-fish
 # bin/install --offline
-# fish -c "omf install bobthefish"
+# sudo fish -c "omf install bobthefish"
 
 
 # sudo apt install curl
-# fish -c "sudo snap install --classic code"
+# sudo fish -c "sudo snap install --classic code"
 # curl -L github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 
 # touch ~/.config/fish/functions/su.fish
