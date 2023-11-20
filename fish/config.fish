1146b7cd2778a013d84700c0f44ef95934bf2cd8
Reza Hidayat <m.rezahidayat.rh@gmail.com>
2023-11-20 10:22:53 +0700

    update script

--- fish/config.fish
+++ fish/config.fish
@@ -1,122 +1,143 @@
-set fish_greeting ""
-
-set -gx TERM xterm-256color
-
-# Theme
-set -g theme_color_scheme terminal-dark
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g theme_hostname always
-
-# Aliases
-alias ls "ls -p -G"
-alias la "ls -A"
-alias ll "ls -l"
-alias lla "ll -A"
-alias g git
-command -qv nvim && alias vim nvim
-
-set -gx EDITOR nvim
-
-set -gx PATH bin $PATH
-set -gx PATH ~/bin $PATH
-set -gx PATH ~/.local/bin $PATH
-
-# NodeJS (jika diperlukan)
-# set -gx PATH node_modules/.bin $PATH
-
-# Go (jika diperlukan)
-# set -g GOPATH $HOME/go
-# set -gx PATH $GOPATH/bin $PATH
-
-function fish_user_key_bindings
-  # fzf
-  bind \cf fzf_change_directory
-
-  # vim-like
-  bind \cl forward-char
-
-  # prevent iterm2 from closing when typing Ctrl-D (EOF)
-  bind \cd delete-char
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 end
 
-# Contoh fungsi untuk melakukan operasi khusus
-function my_custom_function
-    # Isi fungsi di sini
-    echo "Hello from my_custom_function!"
+# custom reza pace
+alias g='cd /home/r/github'
+
+# alias
+alias profile='code ~/.config/fish/config.fish'
+alias j='z'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='trash -v'
+alias mkdir='mkdir -p'
+alias ps='ps auxf'
+alias ping='ping -c 10'
+alias less='less -R'
+alias c='clear'
+alias apt-get='sudo apt-get'
+alias multitail='multitail --no-repeat -c'
+alias freshclam='sudo freshclam'
+alias vi='nvim'
+alias svi='sudo vi'
+alias vis='nvim "+set si"'
+alias home='cd ~'
+
+# cd into the old directory
+alias bd='cd "$OLDPWD"'
+
+# Remove a directory and all files
+alias rmd='/bin/rm  --recursive --force --verbose '
+
+# Alias's for multiple directory listing commands
+alias la='ls -Alh' # show hidden files
+alias ls='ls -aFh --color=always' # add colors and file type extensions
+alias lx='ls -lXBh' # sort by extension
+alias lk='ls -lSrh' # sort by size
+alias lc='ls -lcrh' # sort by change time
+alias lu='ls -lurh' # sort by access time
+alias lr='ls -lRh' # recursive ls
+alias lt='ls -ltrh' # sort by date
+alias lm='ls -alh |more' # pipe through 'more'
+alias lw='ls -xAh' # wide listing format
+alias ll='exa --header --long' # long listing format
+alias labc='ls -lap' #alphabetical sort
+alias lf="ls -l | egrep -v '^d'" # files only
+alias ldir="ls -l | egrep '^d'" # directories only
+
+# alias chmod commands
+alias mx='chmod a+x'
+alias 000='chmod -R 000'
+alias 644='chmod -R 644'
+alias 666='chmod -R 666'
+alias 755='chmod -R 755'
+alias 777='chmod -R 777'
+
+# Search command line history
+alias h="history | grep "
+
+# Search running processes
+alias p="ps aux | grep "
+alias topcpu="/bin/ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10"
+
+# Search files in the current folder
+alias f="find . | grep "
+
+# To see if a command is aliased, a file, or a built-in command
+alias checkcommand="type -t"
+
+# Show open ports
+alias openports='netstat -nape --inet'
+
+# Alias's for safe and forced reboots
+alias rebootsafe='sudo shutdown -r now'
+alias rebootforce='sudo shutdown -r -n now'
+
+# SHA1
+alias sha1='openssl sha1'
+# KITTY - alias to be able to use kitty features when connecting to remote servers(e.g use tmux on remote server)
+
+alias kssh="kitty +kitten ssh"
+
+# melist file setelah cd
+function cd -d "Change directory and list contents"
+    if test -n "$argv"
+        builtin cd $argv; and exa --header --long
+    else
+        builtin cd ~; and exa --header --long
+    end
 end
 
-# Fungsi untuk menjalankan XAMPP
-function xampp_run
-    echo "Starting XAMPP..."
+# membuatfile 
+function mkdirg -d "Create directory and change into it"
+    mkdir -p $argv[1]
+    cd $argv[1]
+end
+
+# GitHub Titus Additions
+function gcom -d "Add all changes and commit with message"
+    git add .
+    git commit -m $argv[1]
+end
+
+function gup -d "Add all changes, commit and push with message"
+    git add .
+    git commit -m $argv[1]
+    git push
+end
+
+function xampprun -d "Start XAMPP"
     sudo service httpd start
     sudo service mariadb start
 end
 
-# Fungsi untuk melihat status XAMPP
-function xampp_status
-    echo "Showing XAMPP status..."
+function xamppstat -d "Show XAMPP status"
     sudo systemctl status httpd.service
     sudo systemctl status mariadb.service
 end
 
-# Fungsi untuk menghentikan XAMPP
-function xampp_stop
-    echo "Stopping XAMPP..."
+function xamppstop -d "Stop XAMPP"
     sudo service httpd stop
     sudo service mariadb stop
 end
 
-# Fzf
-set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
-set -g FZF_LEGACY_KEYBINDINGS 0
-set -g FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border --preview-window=right:60% --preview 'bat --style=numbers --color=always --line-range :500 {}'"
-set -g FZF_CTRL_T_COMMAND "fd --type f --hidden --follow --exclude .git --exclude node_modules --exclude .cache"
-set -g FZF_CTRL_T_OPTS "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
-set -g FZF_ALT_C_COMMAND "fd --type d --hidden --follow --exclude .git --exclude node_modules --exclude .cache"
-set -g FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
-set -g FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git --exclude node_modules --exclude .cache"
+function htdoc -d "Change directory to /var/www/html"
+    cd /var/www/html/
+end
 
-
+# Use Peco to search command history with CTRL+h
 function fish_user_key_bindings
-  # fzf
-  bind \cf fzf_change_directory
-
-  # vim-like
-  bind \cl forward-char
-
-  # prevent iterm2 from closing when typing Ctrl-D (EOF)
-  bind \cd delete-char
+    bind \cr 'history | peco'
+    bind \cf 'ls | peco'
 end
 
-# fzf plugin
-fzf_configure_bindings --directory=\co
-fzf_configure_bindings --directory=\co
-fzf_configure_bindings --file=\cf
-fzf_configure_bindings --git=\cg
-fzf_configure_bindings --history=\ch
-fzf_configure_bindings --jump=\cj
-fzf_configure_bindings --process=\ck
-fzf_configure_bindings --search=\cs
-fzf_configure_bindings --shell=\c'
-
-function _fzf_change_directory
-  fzf | perl -pe 's/([ ()])/\\\\$1/g'|read foo
-  if [ $foo ]
-    builtin cd $foo
-    commandline -r ''
-    commandline -f repaint
-  else
-    commandline ''
-  end
+function vs
+    set -l argument $argv[1]
+    j $argument
+    code .
 end
 
-function fzf_change_directory
-  begin
-    echo $HOME/.config
-    find $(ghq root) -maxdepth 4 -type d -name .git | sed 's/\/\.git//'
-    ls -ad */|perl -pe "s#^#$PWD/#"|grep -v \.git
-    ls -ad $HOME/Developments/*/* |grep -v \.git
-  end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
+function e
+    xdg-open .
 end
--- script/fish.sh
+++ script/fish.sh
@@ -6,33 +6,14 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-# Restart environment
-source ~/.bashrc
-
 # Install Fish Shell
+sudo apt install curl -y
 apt-get install fish -y
 
-# Install Plugin dengan fish
-fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
-fish -c "fisher install jorgebucaran/nvm.fish"
-fish -c "fisher install ilancosman/tide@v4.1.1"
-fish -c "fisher install jethrokuan/z"
-fish -c "fisher install takashabe/fish-peco"
-fish -c "fisher install PatrickF1/fzf.fish"
-fish -c "fisher install simnalamburt/shellder"
-
-
-# Download dan install exa
-wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
-apt-get install ./exa_0.9.0-4_amd64.deb -y
-
-# Clone repository ghq
-git clone https://github.com/x-motemen/ghq .
-
 # Pindah ke direktori ghq dan buat file konfigurasi
-cd ghq
+mkdir -p ~/.config/fish
+cp /home/r/github/LINUX/script/config.fish ~/.config/fish/config.fish
 touch ~/.config/fish/functions/su.fish
-touch ~/.config/fish/config.fish
 touch ~/.config/fish/functions/fish_prompt.fish
 
 # Set permission
@@ -40,13 +21,25 @@ chmod 700 ~/.config/fish/functions/su.fish
 chmod 700 ~/.config/fish/config.fish
 chmod 700 ~/.config/fish/functions/fish_prompt.fish
 
-# Hapus semua file yang telah diinstal (opsional)
-# rm -f fish mimedb fish_pager fishd fish_indent
-# rm /usr/local/share/man/man1/fish*.1
-# rm -Rf /usr/local/etc/fish /usr/local/share/fish ~/.config/fish
+# Install Plugin dengan fish
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+fish -c "fisher install jorgebucaran/nvm.fish"
+fish -c "fisher install jethrokuan/z"
+fish -c "fisher install takashabe/fish-peco"
+fish -c "fisher install edc/bass"
+fish -c "fisher install PatrickF1/fzf.fish"
+fish -c "fisher install simnalamburt/shellder"
+fish -c "sudo apt install exa"
+fish -c "sudo apt install peco"
+fish -c "sudo apt install fzf"
+fish -c "sudo apt install trash-cli"
 
 echo "Script selesai dijalankan."
 
 # sudo apt install curl
-
+# fish -c "sudo snap install --classic code"
 # curl -L github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
+
+# touch ~/.config/fish/functions/su.fish
+# touch ~/.config/fish/config.fish
+# touch ~/.config/fish/functions/fish_prompt.fish
